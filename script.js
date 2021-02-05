2d50119f4bbca7770c81f5c2c59e667e0dea8f55
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-02-05 20:27:35 +0100

    ajout du js pour le texte qui s'écrit au fur et à mesure

---
+++ script.js
@@ -0,0 +1,5 @@
+// TYPED
+var typed = new Typed('.typed', {
+    strings: ["Bonjour à tous et à toutes, je me présente je m'appelle Mohamed", "Après une carrière m'ayant fait découvrir plusieurs milieux professionnels et exercer plus de 5 métiers différents, j'ai décidé de me lancer à fond dans l'apprentissage du code, depuis près de 18 mois je me forme à temps plein en autodidacte aux languages Front tels que CSS, Javascript et le Framework VueJs, ainsi qu'aux languages Back tels que SQL, PHP et le Framework Symfony et Codeigniter (depuis peu) afin de devenir le plus rapidement développeur. Plus qu'un avenir professionel, je me suis découvert une passion, celle de coder"],
+    typeSpeed: 20
+});
