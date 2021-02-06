fb29103d8a958c99b19af6159be3d5c2de5704d6
guerroui.m@gmail.com <guerroui.m@gmail.com>
2021-02-06 20:32:11 +0100

    ajout du js pour les langages et les frameworks

--- script.js
+++ script.js
@@ -20,3 +20,32 @@ var typed = new Typed('.typed', {
     strings: ["Bonjour à tous et à toutes, je me présente je m'appelle Mohamed", "Après une carrière m'ayant fait découvrir plusieurs milieux professionnels et exercer plus de 5 métiers différents, j'ai décidé de me lancer à fond dans l'apprentissage du code, depuis près de 18 mois je me forme à temps plein en autodidacte aux languages Front tels que CSS, Javascript et le Framework VueJs, ainsi qu'aux languages Back tels que SQL, PHP et le Framework Symfony et Codeigniter (depuis peu) afin de devenir le plus rapidement développeur. Plus qu'un avenir professionel, je me suis découvert une passion, celle de coder"],
     typeSpeed: 20
 });
+
+// COMPTEUR LIVE
+let compteur = 0;
+
+$(window).scroll(function() {
+    const top = $('.counter').offset().top - window.innerHeight;
+    if(compteur == 0 && $(window).scrollTop() > top) {
+        $('.counter-value').each(function() {
+            let $this = $(this),
+            countTo = $this.attr('data-count');
+            $({
+                countNum: $this.text()
+            }).animate({
+                countNum : countTo
+            },
+            {
+                duration: 5000,
+                easing: 'swing',
+                step: function() {
+                    $this.text(Math.floor(this.countNum));
+                },
+                complete: function() {
+                    $this.text(this.countNum);
+                }
+            });
+        });
+        compteur = 1;
+    }
+});
